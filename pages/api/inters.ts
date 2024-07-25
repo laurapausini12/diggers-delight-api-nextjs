ac124ce7337eee1e49735f862b309e777b20ddc5
mignot <andreas.garcia@bodzify.com>
2024-07-25 16:24:13 +0200

    Use db

--- pages/api/inters.ts
+++ pages/api/inters.ts
@@ -1,32 +1,65 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
+import { sql } from "@vercel/postgres";
 
-const INTERS_CSV_FILENAME = path.join(os.tmpdir(), "inters.csv");
+const INTERS_RELATION_NAME = "inters";
 
 function getInters(req: NextApiRequest, res: NextApiResponse) {
-  if (!fs.existsSync(INTERS_CSV_FILENAME)) {
-    res.status(200).json({ count: 0, result: null });
-    return;
-  }
-
-  const inters = fs.readFileSync(INTERS_CSV_FILENAME, "utf-8");
-  res.status(200).json({ count: inters.length, result: inters });
+  const query = `SELECT * FROM ${INTERS_RELATION_NAME}`;
+  sql
+    .query(query)
+    .then((result) => {
+      res.status(200).json(result.rows);
+    })
+    .catch((error) => {
+      if (error instanceof Error) {
+        res
+          .status(500)
+          .json({ error: `Failed to fetch inters: ${error.message}` });
+      } else {
+        res
+          .status(500)
+          .json({ error: "Failed to fetch inters: Unknown error" });
+      }
+    });
 }
 
-function postInters(req: NextApiRequest, res: NextApiResponse) {
+function createIntersTableIfNotExists() {
+  return sql`
+    CREATE TABLE IF NOT EXISTS ${INTERS_RELATION_NAME} (
+      address TEXT PRIMARY KEY,
+      time NUMERIC
+    )
+  `;
+}
+
+function emptyIntersTable() {
+  const query = `DELETE * FROM ${INTERS_RELATION_NAME}`;
+  sql.query(query);
+}
+
+async function postInters(req: NextApiRequest, res: NextApiResponse) {
   const inters: { address: string; time: number }[] = req.body;
-  const intersCsv = inters
-    .map((inter) => `${inter.address},${inter.time}`)
-    .join("\n");
-
-  if (!fs.existsSync(INTERS_CSV_FILENAME)) {
-    fs.writeFileSync(INTERS_CSV_FILENAME, "");
+  const values = inters.map((inter) => [inter.address, inter.time]);
+  createIntersTableIfNotExists();
+  emptyIntersTable();
+  try {
+    for (const value of values) {
+      const query = `
+        INSERT INTO ${INTERS_RELATION_NAME} (address, time) 
+        VALUES ('${value[0]}', ${value[1]})
+      `;
+      await sql.query(query);
+    }
+    res.status(200).json({ message: "Inters saved successfully" });
+  } catch (error) {
+    if (error instanceof Error) {
+      res
+        .status(500)
+        .json({ error: `Failed to save inters: ${error.message}` });
+    } else {
+      res.status(500).json({ error: "Failed to save inters: Unknown error" });
+    }
   }
-
-  fs.writeFileSync(INTERS_CSV_FILENAME, intersCsv);
-  res.status(200).json({ message: "Inters saved successfully" });
 }
 
 export default async function handler(
